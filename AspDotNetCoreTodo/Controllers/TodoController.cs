faaa3bc29ccf5606c916e827f8482dd1dc9a8736
Phillip Dodson <clevergadget@gmail.com>
2019-05-16 18:09:36 -0400

    added models

--- AspDotNetCoreTodo/Controllers/TodoController.cs
+++ AspDotNetCoreTodo/Controllers/TodoController.cs
@@ -7,6 +7,11 @@ namespace AspNetCoreTodo.Controllers
 {
     public class TodoController : Controller
     {
-        // Actions go here
+        public IActionResult Index()
+        {
+            // Get to-do items from database
+            // Put items into a model
+            // Render view using the model
+        }
     }
 }
---
+++ AspDotNetCoreTodo/Models/TodoViewModel.cs
@@ -0,0 +1,7 @@
+namespace AspDotNetCoreTodo.Models
+{
+    public class TodoViewModel
+    {
+        public TodoItem[] Items { get; set; }
+    }
+}
